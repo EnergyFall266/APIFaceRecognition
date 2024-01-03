cb1636ac162a22a879cf8de84259cfbeea13ac9f
Leonardo Vanzin <leonardo.vanzin@hotmail.com>
2024-01-03 09:31:14 -0300

    reconhecimento

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import io
 import face_recognition
 import os
 class Img(BaseModel):
-    url: str | None = None
-    name: str | None = None
+    url: str 
+    name: str 
 
 app = FastAPI()
 known_faces = []
@@ -22,11 +22,11 @@ async def create_image(image: Img):
     imag.convert('RGB')
     imag.save(f"{image.name}.jpg")
     img2 = cv2.imread(f"{image.name}.jpg")
-    rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
-    img_encoding2 = face_recognition.face_encodings(rgb_img2)[0]
+    rgb_img = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+    img_encoding = face_recognition.face_encodings(rgb_img)[0]
     os.remove(f"{image.name}.jpg")
     lista.append(image.name)
-    lista.append(img_encoding2)
+    lista.append(img_encoding)
     known_faces.append(lista)
     print(known_faces)
     print(len(known_faces))
@@ -37,14 +37,22 @@ async def create_image(image: Img):
 
 @app.post("/Reconhecimento")
 async def create_image(image: Img):
+    img = base64.b64decode(image.url)
+    imag = Image.open(io.BytesIO(img))
+    imag.convert('RGB')
+    imag.save(f"{image.name}.jpg")
+    img2 = cv2.imread(f"{image.name}.jpg")
+    rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+    img_encoding2 = face_recognition.face_encodings(rgb_img2)[0]
+    os.remove(f"{image.name}.jpg")
     i=0
     while(i<len(known_faces)):
         result = face_recognition.compare_faces([known_faces[i][1]], img_encoding2)
         if(result[0]):
-            print("Result: ", result)
-            print("Distance: ", face_recognition.face_distance([known_faces[i][1]], img_encoding2))
-            print("Name: ", known_faces[i][0])
-            break
+            return {"message": "Person found",
+                    "name": known_faces[i][0],
+                    "distance": face_recognition.face_distance([known_faces[i][1]], img_encoding2)[0]}
+            
         i+=1
-        if not result[0]:
-            print("Not found")
+    if not result[0]:
+        return {"message": "Person not found"}
